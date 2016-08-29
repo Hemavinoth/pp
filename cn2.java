1af9c536d739938cff6fc17dada78d32a173b861
hemalatha <avhemavinoth@gmail.com>
2016-08-29 18:31:24 +0530

    Update and rename cn.java to cn2.java

---
+++ cn2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+class cn2{
+public static void main(String args[]){
+int i;
+String s;
+Scanner sc=new Scanner(System.in);
+s=sc.next();
+i=Integer.parseInt(s);
+System.out.println(" "+i);
+}
+}
+		
+		
