edd905bee5ebd10440f69b1521e55a3ab131a068
hemalatha <avhemavinoth@gmail.com>
2016-08-29 19:02:58 +0530

    Update cn.java

--- cn.java
+++ cn.java
@@ -1,19 +1,21 @@
 
-import java.util.Arrays; import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class cn {
 
-public static void main(String[] args) {
-    Scanner qq = new Scanner(System.in);
-    System.out.println("enter size of array");
-    int nn = qq.nextInt();
-    int count[] = new int[nn];
-    System.out.println("enter array element");
-    for (int i = 0; i < nn; i++) {
-        count[i] = qq.nextInt();
+public static void main(String[] args)
+{
+    Scanner q = new Scanner(System.in);
+    System.out.println("enter");
+    int nn = q.nextInt();
+    int c[] = new int[n];
+    System.out.println("array");
+    for (int i = 0; i < n; i++) {
+        c[i] = qq.nextInt();
     }
-    Arrays.sort(count);
-    System.out.println("The Second Smallest number is ");
-    System.out.println(count[1]);
+    Arrays.sort(c);
+    System.out.println("small is ");
+    System.out.println(c[1]);
 }
 }
