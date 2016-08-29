ffb4aefc113c49dce9188a61964cc5743a0b4508
hemalatha <avhemavinoth@gmail.com>
2016-08-29 19:04:01 +0530

    Update cn.java

--- cn.java
+++ cn.java
@@ -7,15 +7,13 @@ public class cn {
 public static void main(String[] args)
 {
     Scanner q = new Scanner(System.in);
-    System.out.println("enter");
     int nn = q.nextInt();
     int c[] = new int[n];
-    System.out.println("array");
     for (int i = 0; i < n; i++) {
-        c[i] = qq.nextInt();
+        c[i] = q.nextInt();
     }
     Arrays.sort(c);
-    System.out.println("small is ");
+    
     System.out.println(c[1]);
 }
 }
